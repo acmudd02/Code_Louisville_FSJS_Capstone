83ace7c54c04227c21e326f34dbc8dce70111fe0
Adam <Adam@DESKTOP-2N1S0HO>
2017-06-28 10:33:13 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const bodyParser= require('body-parser')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.set('view engine', 'ejs')
+//response.render(view, locals)
+
+app.get('/', (req, res) => {
+  db.collection('quotes').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('index.ejs', {quotes: result})
+  })
+})
+
+app.post('/quotes', (req, res) => {
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
+
+
+var db
+
+MongoClient.connect('mongodb://darth_plagueis_the_wise:!237mad6m9@ds141082.mlab.com:41082/star_wars_quotes', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
